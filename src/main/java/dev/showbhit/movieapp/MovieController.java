0eab8960662a010fa07db7f8738b9274159c6a96
shobhitm23 <shobhitm05@gmail.com>
2023-03-11 15:09:07 -0800

    added CORS support to getMovies

--- src/main/java/dev/showbhit/movieapp/MovieController.java
+++ src/main/java/dev/showbhit/movieapp/MovieController.java
@@ -4,10 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +17,7 @@ public class MovieController {
     private MovieService movieService;
 
     @GetMapping
+    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
